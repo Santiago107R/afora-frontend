b2fc3ac3a6a4d267e2613a5e206670490635b2f4
Santiago107R <santiagorobles107k@gmail.com>
2026-07-22 10:55:51 -0300

    fix: wrap CustomHeader and CustomFooter in divs for improved layout structure

--- src/user/layouts/UserLayout.tsx
+++ src/user/layouts/UserLayout.tsx
@@ -10,25 +10,26 @@ const UserLayout = () => {
   return (
     <div className="h-screen w-full grid grid-cols-1 grid-rows-[auto_1fr_auto]">
 
-      <CustomHeader
-        title={"Afora"}
-        link={[{ name: "Inicio", url: '/' }, { name: "Lista", url: '/user/lista' }]}
-        button={{ onClick: logout, children: "Cerrar Sesión" }}
-        logo={'/favicon.svg'}
-        icon={User2}
-      />
+      <div className="h-fit w-full">
+        <CustomHeader
+          title={"Afora"}
+          link={[{ name: "Inicio", url: '/' }, { name: "Lista", url: '/user/lista' }]}
+          button={{ onClick: logout, children: "Cerrar Sesión" }}
+          logo={'/favicon.svg'}
+          icon={User2}
+        />
+      </div>
 
       <main className="min-h-0 w-full h-full overflow-y-auto">
         <Outlet />
       </main>
 
-      <CustomFooter
-        description="Derechos reservados por Afora"
-        link={[
-          { name: "Arturitorodriguezvera@gmail.com", url: "#" },
-          { name: "ezequielnicolasacosta@gmail.com", url: "#" }
-        ]}
-      />
+      <div className="h-fit w-full">
+        <CustomFooter
+          description="Derechos reservados por Afora"
+          link={[{ name: "Arturitorodriguezvera@gmail.com", url: "#" }, { name: "ezequielnicolasacosta@gmail.com", url: "#" }]}
+        />
+      </div>
 
     </div>
   )
