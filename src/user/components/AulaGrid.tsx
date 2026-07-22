69058c236b03d7852df3bee945698d769a4058a1
Santiago107R <santiagorobles107k@gmail.com>
2026-07-22 10:34:14 -0300

    fix: enhance layout structure in AulaGrid and UserLayout for improved consistency and aesthetics

--- src/user/components/AulaGrid.tsx
+++ src/user/components/AulaGrid.tsx
@@ -9,19 +9,17 @@ const AulaGrid = ({ aulas }: Props) => {
 
     if (!aulas || aulas.length == 0) {
         return (
-            <div className="flex justify-center items-center">
-                <p>No hay aulas cargadas</p>
+            <div className="h-full w-full flex justify-center items-center">
+                <p className="text-gray-500 font-medium">No hay aulas cargadas</p>
             </div>
         )
     }
 
     return (
         <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-8">
-
             {aulas.map((aula) => (
                 <AulaCard key={aula.name} name={aula.name} capacity={aula.capacity} state={aula.state} clase={aula.clase} />
             ))}
-
         </div>
     )
 }
--- src/user/layouts/UserLayout.tsx
+++ src/user/layouts/UserLayout.tsx
@@ -4,24 +4,33 @@ import CustomHeader from "../../components/CustomHeader"
 import { User2 } from "lucide-react"
 import { useAuthStore } from "@/auth/store/auth.store"
 
-
 const UserLayout = () => {
   const { logout } = useAuthStore()
+
   return (
     <div className="h-screen w-full grid grid-cols-1 grid-rows-[auto_1fr_auto]">
-      {/* <div className="flex flex-col min-h-screen"> */}
-      {/* {name: "Cerrar Sesión", url: '/logout',} */}
-      <CustomHeader
-        title={"Afora"}
-        link={[{ name: "Inicio", url: '/' }, { name: "Lista", url: '/user/lista' }]}
-        button={{ onClick: logout, children: "Cerrar Sesión" }}
-        logo={'/favicon.svg'}
-        icon={User2}
-      />
+
+      <div className="h-fit w-full">
+        <CustomHeader
+          title={"Afora"}
+          link={[{ name: "Inicio", url: '/' }, { name: "Lista", url: '/user/lista' }]}
+          button={{ onClick: logout, children: "Cerrar Sesión" }}
+          logo={'/favicon.svg'}
+          icon={User2}
+        />
+      </div>
+
       <main className="min-h-0 w-full h-full">
         <Outlet />
       </main>
-      <CustomFooter description="Derechos reservados por Afora" link={[{ name: "Arturitorodriguezvera@gmail.com", url: "#" }, { name: "ezequielnicolasacosta@gmail.com", url: "#" }]} />
+
+      <div className="h-fit w-full">
+        <CustomFooter
+          description="Derechos reservados por Afora"
+          link={[{ name: "Arturitorodriguezvera@gmail.com", url: "#" }, { name: "ezequielnicolasacosta@gmail.com", url: "#" }]}
+        />
+      </div>
+
     </div>
   )
 }
