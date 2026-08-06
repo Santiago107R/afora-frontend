04e9bd52df932beca11d6f5f5408fb664fa4d44b
ezee131415 <ezequielnicolasacosta@gmail.com>
2026-08-06 19:15:03 -0300

    fix: update layout and styling of CustomFooter component for improved structure and readability

--- src/components/CustomFooter.tsx
+++ src/components/CustomFooter.tsx
@@ -1,5 +1,6 @@
 import type { LinkProp } from "@/user/types/links";
 import { Link } from "react-router";
+import { Mail, Phone, MapPin } from "lucide-react";
 
 interface Props {
   description: string;
@@ -8,18 +9,46 @@ interface Props {
 
 const CustomFooter = ({ description, link }: Props) => {
   return (
-    <footer className="bg-[#1A2B3C] border-t-2 p-5 text-white">
+    <footer className="bg-black border-t-2 border-gray-300 text-white px-5 py-2">
+      
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 items-center gap-4">
 
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-3">
+        <div className="text-sm">
+          <p className="mb-2 text-gray-400 ">...</p>
 
-        <p className="text-gray-400 text-sm">
-          &copy; {description}
-        </p>
+          <p className= "text-gray-400">
+            &copy; {description}
+          </p>
+        </div>
+        
+        <div className="text-sm">
+          <p className="text-base mb-1">
+            Contacto
+          </p>
 
-        <div className="flex flex-col items-end">
+          <div className="flex items-center gap-1 text-xs mb-1">
+            <Mail className="w-4 h-4" />
+            <span>figma@gmail.com</span>
+          </div>
 
-          {link && link.map((item) => (
-            <Link key={item.name} to={item.url} className="hover:text-blue-200">
+          <div className="flex items-center gap-1 text-xs mb-1">
+            <Phone className="w-4 h-4" />
+            <span>11 5687 9802</span>
+          </div>
+
+          <div className="flex items-center gap-1 text-xs">
+            <MapPin className="w-4 h-4" />
+            <span>La Matanza, Argentina</span>
+          </div>
+        </div>
+
+        <div className="flex justify-start md:justify-end gap-6 text-xs">
+          {link?.map((item) => (
+            <Link
+              key={item.name}
+              to={item.url}
+              className="hover:text-(--color-thirty) transition-colors whitespace-nowrap"
+            >
               {item.name}
             </Link>
           ))}
@@ -28,7 +57,7 @@ const CustomFooter = ({ description, link }: Props) => {
       </div>
 
     </footer>
-  )
-}
+  );
+};
 
-export default CustomFooter
+export default CustomFooter;
