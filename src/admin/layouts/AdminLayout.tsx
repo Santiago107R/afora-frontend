989eef18783a6067703a49fe99f34bcd9de436e1
Santiago107R <santiagorobles107k@gmail.com>
2026-08-08 18:06:38 +0000

    fix: add TODO comment for sidebar implementation in AdminLayout

--- src/admin/layouts/AdminLayout.tsx
+++ src/admin/layouts/AdminLayout.tsx
@@ -18,6 +18,8 @@ const AdminLayout = () => {
                 />
             </div>
 
+            //TODO: SIDEBAR ACÁ
+
             <main className="min-h-0 w-full h-full overflow-y-auto">
                 <Outlet />
             </main>
