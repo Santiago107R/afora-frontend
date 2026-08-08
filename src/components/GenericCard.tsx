17f6d711a452c15ad485823b3c3a925164eaf501
Santiago107R <santiagorobles107k@gmail.com>
2026-08-08 17:58:12 +0000

    fix: update layout and styling in AulasAdminGrid and GenericCard for improved consistency and spacing

--- src/components/GenericCard.tsx
+++ src/components/GenericCard.tsx
@@ -17,27 +17,27 @@ const GenericCard = ({
     title,
     children,
     actions,
-    className = "w-full h-full flex flex-col bg-(--color-white-primary) border border-(--color-black-primary) rounded-md p-3",
+    className = "w-full max-w-[280px] mx-auto h-full flex flex-col bg-(--color-white-primary) border border-(--color-black-primary) rounded-md p-2.5 shadow-sm",
 }: GenericCardProps) => {
     return (
         <div className={className}>
-            {title && <h2 className="text-2xl text-center">{title}</h2>}
+            {title && <h2 className="text-lg font-semibold text-center">{title}</h2>}
 
-            <div className="flex-1 mt-2 space-y-1">
+            <div className="flex-1 mt-2 space-y-1 text-sm">
                 {children}
             </div>
 
             {actions && actions.length > 0 && (
-                <div className="flex flex-row justify-between mt-2">
+                <div className="flex flex-row justify-between gap-1 mt-2">
                     {actions.map((action, i) => (
                         <button
                             key={i}
                             type="button"
                             onClick={action.onClick}
-                            className="bg-(--color-primary) p-1 rounded-sm"
+                            className="bg-(--color-primary) px-2 py-1 rounded-sm"
                         >
                             {action.label && (
-                                <span className="font-bold text-(--color-black-fourth)">
+                                <span className="font-bold text-(--color-black-fourth) text-sm">
                                     {action.label}
                                 </span>
                             )}
