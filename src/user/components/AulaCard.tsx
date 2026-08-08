c5deaae1f3641acbe176a4820721d1b82ce69977
Santiago107R <santiagorobles107k@gmail.com>
2026-08-08 20:22:28 +0000

    fix: update AulaCard to use setEntity instead of setSelectedAula for modal functionality

--- src/user/components/AulaCard.tsx
+++ src/user/components/AulaCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 
 const AulaCard = ({ shadow, name, capacity, estado }: Props) => {
-    const { setSelectedAula: openModal } = useSelectedAulaStore()
+    const { setEntity } = useSelectedAulaStore()
 
     // const statusColors = {
     //     available: "text-green-500",
@@ -22,7 +22,7 @@ const AulaCard = ({ shadow, name, capacity, estado }: Props) => {
     return (
         <>
             <div
-                onClick={() => openModal({ name })}
+                onClick={() => setEntity({ name })}
                 className={` flex flex-col py-5 px-1 bg-[#D9D9D9] rounded-2xl w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-lg cursor-pointer transition hover:scale-105 border-[#D9D9D9] hover:border-[#3B78B3] border-2 overflow-hidden ${shadow && 'shadow-[10px_10px_7px_rgba(0,0,0,0.25)]'} `}
             >
 
