3dfe1e33b6949b95bafc647c1715d81bb0959395
Santiago107R <santiagorobles107k@gmail.com>
2026-08-08 15:15:45 +0000

    fix: update AulaAdminCard styles for improved layout and consistency

--- src/admin/components/AulaAdminCard.tsx
+++ src/admin/components/AulaAdminCard.tsx
@@ -8,17 +8,17 @@ interface Props {
 
 const AulaAdminCard = ({ name, capacity, estado }: Props) => {
     return (
-        <div>
+        <div className="bg-(color-white-primary) border border-(--color-black-primary)">
             <h1>{name}</h1>
             <p>Capacidad: {capacity}</p>
             <p>Estado: {estado}</p>
 
             <div className="flex flex-row gap-10">
-                <div className="bg-(--color-primary) p-3">
-                    <p>Detalles</p>
+                <div className="bg-(--color-primary) py-1 px-2 rounded-sm">
+                    <p className="font-bold">Detalles</p>
                 </div>
 
-                <div className="bg-(--color-primary) p-3">
+                <div className="bg-(--color-primary) py-1 px-2 rounded-sm">
                     <p><Edit /></p>
                 </div>
             </div>
