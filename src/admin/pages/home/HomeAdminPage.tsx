d752a5712a55aa06c32f248461fea940fdff88e8
Santiago107R <santiagorobles107k@gmail.com>
2026-08-08 18:09:57 +0000

    fix: add navigation button to redirect to Aulas page in HomeAdminPage

--- src/admin/pages/home/HomeAdminPage.tsx
+++ src/admin/pages/home/HomeAdminPage.tsx
@@ -1,12 +1,15 @@
 import { useAuthStore } from "@/auth/store/auth.store"
 import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router"
 
 const HomeAdminPage = () => {
     const { logout } = useAuthStore()
+    const navigate = useNavigate()
 
     return (
         <div>
             <Button variant={"destructive"} onClick={logout}>Salir</Button>
+            <Button variant={"default"} onClick={() => navigate('/aulas')}>Salir</Button>
         </div>
     )
 }
