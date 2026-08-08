2d74f688429ebd9ee13cb5271234f276c39c996c
Santiago107R <santiagorobles107k@gmail.com>
2026-08-08 15:33:24 +0000

    fix: correct heading level in AulaAdminCard and enhance text color for Detalles

--- src/admin/components/AulaAdminCard.tsx
+++ src/admin/components/AulaAdminCard.tsx
@@ -9,13 +9,13 @@ interface Props {
 const AulaAdminCard = ({ name, capacity, estado }: Props) => {
     return (
         <div className="bg-(color-white-primary) border border-(--color-black-primary) rounded-md p-3">
-            <h1 className="text-xl text-center">{name}</h1>
+            <h2 className="text-xl text-center ">{name}</h2>
             <p>Capacidad: {capacity}</p>
             <p>Estado: {estado}</p>
 
             <div className="flex flex-row gap-5 mt-3">
                 <div className="bg-(--color-primary) p-1 rounded-sm">
-                    <p className="font-bold ">Detalles</p>
+                    <p className="font-bold text-(--color-black-fourth)">Detalles</p>
                 </div>
 
                 <div className="bg-(--color-primary) p-1 rounded-sm">
